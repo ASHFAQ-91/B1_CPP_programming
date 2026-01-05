78fb4bdddb7098d6950326cdcedfafc730fc2028
Ashfaq-91 <ashfaqqureshi242@gmail.com>
2026-01-05 23:34:50 +0400

    9append-string.cpp

---
+++ c++_string/9append-string.cpp
@@ -0,0 +1,13 @@
+// Attach, Concate or Append the two string .
+#include<iostream>
+using namespace std;
+int main()
+{
+    string s1 = "fam", s2="ily";
+
+    s1.append(s2);                  //  1st option
+    cout << s1 << endl;
+
+    // s1 = s1 + s2;                //  2nd option
+    // cout << s1 << endl;          //  2nd option    
+}
