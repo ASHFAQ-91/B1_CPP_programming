bb471643cbd9724bdee90a07154aea4e4d3e0a6b
Ashfaq-91 <ashfaqqureshi242@gmail.com>
2026-01-05 23:34:15 +0400

    8access-a-perticular-char.cpp

---
+++ c++_string/8access-a-perticular-char.cpp
@@ -0,0 +1,14 @@
+// access a paticular character in an array.
+#include<iostream>
+using namespace std;
+int main()
+{
+    // string str = "MASQ";
+    // str.clear();                 // this function clear the string
+    // cout << str[1];
+
+    string str1;
+    cout << "Enter your name :  ";
+    getline(cin, str1);
+    cout << str1[22];
+}
