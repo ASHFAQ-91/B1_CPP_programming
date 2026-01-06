0f8933154caeb71ade7754bdab29666b094d080c
Ashfaq-91 <ashfaqqureshi242@gmail.com>
2026-01-06 23:47:55 +0400

    13find-letters-in-string.cpp

---
+++ c++_string/13find-letters-in-string.cpp
@@ -0,0 +1,9 @@
+// find the location of Letter or Word are placed in the string
+#include<iostream>
+using namespace std;
+int main()
+{
+    string str = "Mohammed";
+
+    cout << str.find("me") << endl;
+}
