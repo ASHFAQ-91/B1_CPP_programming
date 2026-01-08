23e68594a4381c88efb245cfabda7ffcfbc1099e
Ashfaq-91 <ashfaqqureshi242@gmail.com>
2026-01-08 23:08:47 +0400

    15length-of-string.cpp

---
+++ c++_string/15length-of-string.cpp
@@ -0,0 +1,11 @@
+// find the length or size of the string.
+#include<iostream>
+using namespace std;
+int main()
+{
+    string str = "mohammed";
+
+    cout << str.size() << endl;         // 1st option
+    
+    cout << str.length() << endl;       // 2nd option
+}
