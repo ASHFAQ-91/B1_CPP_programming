19442aca82eb7e55fc4275d81fbcc53dca98a4c4
Ashfaq-91 <ashfaqqureshi242@gmail.com>
2026-01-09 16:18:37 +0400

    17SHARE-string-to-int.cpp

---
+++ c++_string/17SHARE-string-to-int.cpp
@@ -0,0 +1,11 @@
+// SHARE string number to integer number.
+#include<iostream>
+using namespace std;
+int main()
+{
+    string str = "786";         //string number
+
+    int x = stoi(str);          //share string number to integer number
+
+    cout << x+2 << endl;    
+}
